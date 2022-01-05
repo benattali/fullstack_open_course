1b0c22240e8d6243de9e93b5bdf8085467bf3d86
Ben Attali <ben.attali8@gmail.com>
2022-01-04 21:13:10 -0500

    course information step 7

--- part2/courseinformation/src/App.js
+++ part2/courseinformation/src/App.js
@@ -24,11 +24,19 @@ const Content = ({ course }) => {
   )
 }
 
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((total, part) => total += part.exercises, 0)
+  return(
+    <p>Number of exercises {sum}</p>
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
