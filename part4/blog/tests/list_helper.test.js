34a85d58eacf91dbeed5784ee5028556626e762c
Ben Attali <ben.attali8@gmail.com>
2022-02-22 22:52:24 -0500

    4.4

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -1,8 +1,33 @@
 const listHelper = require("../utils/list_helper")
 
-test("dummy returns one", () => {
-  const blogs = []
+describe("list_helper", () => {
+  test("dummy returns one", () => {
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+
+  describe("totalLikes", () => {
+    test("when a list greater than 1", () => {
+      const blogs = [{ title: "title", likes: 2 }, { title: "title", likes: 3 }]
+
+      const result = listHelper.totalLikes(blogs)
+      expect(result).toBe(5)
+    })
+
+    test("when a list greater is 1", () => {
+      const blogs = [{ title: "title", likes: 2 }]
+
+      const result = listHelper.totalLikes(blogs)
+      expect(result).toBe(2)
+    })
+
+    test("when empty list", () => {
+      const blogs = []
+
+      const result = listHelper.totalLikes(blogs)
+      expect(result).toBe(0)
+    })
+  })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likesList = blogs.map(blog => blog.likes)
+  const likesTotal = likesList.reduce((sum, likes) => sum + likes, 0)
+  return likesTotal
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
