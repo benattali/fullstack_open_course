6cb90a61c8d8a8f86397defe2767ea2fee73e89d
Ben Attali <ben.attali8@gmail.com>
2022-03-13 16:29:25 -0400

    4.16 step 4

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -11,6 +11,18 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: 'password must be longer than 3 characters'
+    })
+  }
+
+  if (!password) {
+    return response.status(400).json({
+      error: 'password must be provided'
+    })
+  }
+
   const existingUser = await User.findOne({ username })
   if (existingUser) {
     return response.status(400).json({
